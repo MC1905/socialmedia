cd5e352081445036af6cfc7bfa89b69d52e73581
MC1905 <94351375+MC1905@users.noreply.github.com>
2023-06-12 11:43:48 +0200

    jhg

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 import { addDoc } from 'firebase/firestore';
 
 
 function App() {
-  const [getTitle, setTitle] = useState("");
-  const [gettextInput, settextInput] = useState("");
-  const [getTask, setTask] = useState("");
-  const [getStatus, setStatus] = useState("To do");
-  const createJob = async () =>{
-    await addDoc
+//   const [getTitle, setTitle] = useState("");
+//   const [gettextInput, settextInput] = useState("");
+//   const [getTask, setTask] = useState("");
+//   const [getStatus, setStatus] = useState("To do");
+//   const createJob = async () =>{
+//     await addDoc
     
-    const add = () => {
+//     const add = () => {
 
-      const document = {
+//       const document = {
   
-        titel: textInput,
+//         titel: textInput,
   
-        omschrijving: ''
+//         omschrijving: ''
   
-      }
+//       }
   
-      const collectie = collection(db, "posts");
+//       const collectie = collection(db, "posts");
   
-      addDoc(collectie, document).then(() => {
+//       addDoc(collectie, document).then(() => {
   
-        console.log('Ik ben klaar!!!');
+//         console.log('Ik ben klaar!!!');
   
-      });
+//       });
   
-    }
-}
+//     }
+// }
 
 
-  return (
-    <div className="App">
+return (
+  <div className="container p-4 ">
+    <div className="row">
+      <Link/>
       
     </div>
-  );
+    <ToastContainer/>
+  </div>
+);
 }
 
 export default App;
