46ca82b99fdbe2c785d03b7be5d874eee3b2a9a9
MC1905 <94351375+MC1905@users.noreply.github.com>
2023-06-20 13:10:35 +0200

    g

--- src/App.js
+++ src/App.js
@@ -1,95 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState } from "react";
-// import { addDoc } from 'firebase/firestore';
+import { useEffect, useState } from "react";
 import { signInWithGoogle } from "./firebase";
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import AuthDetails from './components/AuthDetails';
-import {getDocs,collection,addDoc} from "firebase/firestore";
+import { getDocs, collection, addDoc, onSnapshot } from "firebase/firestore";
 import { db } from "./firebase";
 
-
 function App() {
-//   const [getTitle, setTitle] = useState("");
-//   const [gettextInput, settextInput] = useState("");
-//   const [getTask, setTask] = useState("");
-//   const [getStatus, setStatus] = useState("To do");
-//   const createJob = async () =>{
-//     await addDoc
-    
-//     const add = () => {
+  const [user, setUser] = useState(null);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [posts, setPosts] = useState([]);
 
-//       const document = {
-  
-//         titel: textInput,
-  
-//         omschrijving: ''
-  
-//       }
-  
-//       const collectie = collection(db, "posts");
-  
-//       addDoc(collectie, document).then(() => {
-  
-//         console.log('Ik ben klaar!!!');
-  
-//       });
-  
-//     }
-// }
-  const logout =()=>{
-    localStorage.clear()
-    window.location.reload()
-  }
+  const handleSignInWithGoogle = async () => {
+    try {
+      const userCredential = await signInWithGoogle();
+      const user = userCredential.user;
+      setUser(user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  const handleLogout = () => {
+    setUser(null);
+  };
+
+  const handleAddPost = async () => {
+    try {
+      await addDoc(collection(db, "posts"), {
+        title: title,
+        desc: desc,
+        uid: user.uid,
+      });
+      setTitle("");
+      setDesc("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      const unsubscribe = onSnapshot(collection(db, "posts"), (snapshot) => {
+        const postsData = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setPosts(postsData);
+      });
+
+      return () => unsubscribe();
+    }
+  }, [user]);
 
   return (
     <div className="App">
-      <button class="login-with-google-btn" onClick={signInWithGoogle}>
-        Sign in with Google
-      </button>
+      {!user && (
+        <button className="login-with-google-btn" onClick={handleSignInWithGoogle}>
+          Sign in with Google
+        </button>
+      )}
 
-        <button onClick={AuthDetails.toevoegenDoc}>Toevoegen</button>
+      {user && (
+        <div>
+          <h1>{user.displayName}</h1>
+          <h1>{user.email}</h1>
+          <img src={user.photoURL} alt="Profile" />
+          <button onClick={handleLogout}>Logout</button>
 
-      <h1>{localStorage.getItem("name")}</h1>
-      <h1>{localStorage.getItem("email")}</h1>
-      <img src={localStorage.getItem("profilePic")} />
-      <button onClick={logout}>Logout</button>
-      
+          <div className="add">
+            <input
+              type="text"
+              placeholder="Title"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Desc"
+              value={desc}
+              onChange={(event) => setDesc(event.target.value)}
+            />
+            <button onClick={handleAddPost}>Add Post</button>
+          </div>
+
+          <h2>Posts:</h2>
+          <ul>
+            {posts.map((post) => (
+              <li key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.desc}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
       <div className="App">
-      <SignIn />
-      <SignUp />
-      <AuthDetails />
-
+        <SignIn />
+        <SignUp />
+        <AuthDetails />
+      </div>
     </div>
-
-    
-
-    </div>
-
-    
-
-    
   );
-  const [user, setUser] = useState(null);
-  const [posts, setPosts] = useState([]);
-  const [userid, setId] = useState([]);
-  const [title, setTitle] = useState([]);
-  const [desc, setDesc] = useState([]);
-  const [date, setDate] = useState([]);
-  const [postid, setPostid] = useState();
-
-  const toevoegenDoc = async () => {
-    await addDoc(collection(db, "posts"), {
-      title: title,
-      desc: desc,
-      uid: userid,
-    });
-
-  };
 }
-
-
 
 export default App;
